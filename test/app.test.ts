e9bdf48463da51cc823bace0f7874684af64cba4
Steve Lee <steve@opendirective.com>
2017-05-16 22:10:15 +0100

    remove semicolons

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,19 +1,19 @@
-import { forall, assert, nat, Options } from 'jsverify';
-import { diagramArbitrary, promise } from 'cyclejs-test-helpers';
-import * as htmlLooksLike from 'html-looks-like';
-const toHtml = require('snabbdom-to-html'); //snabbdom-to-html's typings are broken
+import { forall, assert, nat, Options } from 'jsverify'
+import { diagramArbitrary, promise } from 'cyclejs-test-helpers'
+import * as htmlLooksLike from 'html-looks-like'
+const toHtml = require('snabbdom-to-html') //snabbdom-to-html's typings are broken
 
-import xs from 'xstream';
-import { mockDOMSource } from '@cycle/dom';
-import { mockTimeSource } from '@cycle/time';
-import onionify from 'cycle-onionify';
+import xs from 'xstream'
+import { mockDOMSource } from '@cycle/dom'
+import { mockTimeSource } from '@cycle/time'
+import onionify from 'cycle-onionify'
 
-import { App } from '../src/app';
+import { App } from '../src/app'
 
 const testOptions : Options = {
     tests: 100,
     size: 200
-};
+}
 
 describe('app tests', () => {
 
@@ -24,31 +24,31 @@ describe('app tests', () => {
             <button>Increase</button>
             <button>Decrease</button>
         </div>
-    `;
+    `
 
     it('should interact correctly', () => {
         const property = forall(diagramArbitrary, diagramArbitrary, (addDiagram, subtractDiagram) => {
-            const Time = mockTimeSource();
+            const Time = mockTimeSource()
 
-            const add$ = Time.diagram(addDiagram);
-            const subtract$ = Time.diagram(subtractDiagram);
+            const add$ = Time.diagram(addDiagram)
+            const subtract$ = Time.diagram(subtractDiagram)
 
             const DOM = mockDOMSource({
                 '.add': { click: add$ },
                 '.subtract': { click: subtract$ }
-            });
+            })
 
-            const app = onionify(App)({ DOM } as any);
-            const html$ = app.DOM.map(toHtml);
+            const app = onionify(App)({ DOM } as any)
+            const html$ = app.DOM.map(toHtml)
 
             const expected$ = xs.merge(add$.mapTo(+1), subtract$.mapTo(-1))
                 .fold((acc, curr) => acc + curr, 0)
-                .map(expectedHTML);
+                .map(expectedHTML)
 
-            Time.assertEqual(html$, expected$, htmlLooksLike);
-            return promise(Time.run);
-        });
+            Time.assertEqual(html$, expected$, htmlLooksLike)
+            return promise(Time.run)
+        })
 
-        return assert(property, testOptions);
-    });
-});
+        return assert(property, testOptions)
+    })
+})
